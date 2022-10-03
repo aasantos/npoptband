877fc2daa2d80e065c90f050ed12bd97c208c944
António Alberto Santos <aasantos@fe.uc.pt>
2022-10-03 15:35:26 +0100

    Update main.cpp

--- vix/main.cpp
+++ vix/main.cpp
@@ -6,24 +6,25 @@
 //
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
 #include "nprnd.hpp"
 #include "runfunc.hpp"
 
 int main(int argc,const char* argv[])
 {
 	printf("Start .... \n");
+	//
 	double r = 0.02;
 	double tau = 1.0/12.0;
 	//
-	int grid[5] = {16,32,64,128,256};
+	int ngrid = atoi(argv[1]);
+	int nhc = ngrid;
+	int nhc = ngrid;
+	run_in_cpu(0.75, 2.0, nhc, 0.75, 2.0, nhp, 10.0, 47.5, 128, r, tau);
 	//
-	for(int i=0;i<5;i++){
-	   int ngrid = grid[i];
-	   int nhc = ngrid;
-	   int nhp = ngrid;
-	   //
-	   run_in_cpu(0.75, 2.0, nhc, 0.75, 2.0, nhp, 10.0, 47.5, 128, r, tau);
-	}
 	printf("Done ... \n");
 	//
 	return 0;
